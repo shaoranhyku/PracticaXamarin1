8f1cf1686a926bbeb40e1930bd30fbae8af03e4c
shaoranhyku <32264494+shaoranhyku@users.noreply.github.com>
2017-10-24 10:07:49 +0200

    Add project files.

---
+++ Prueba_Xamarin/Prueba_Xamarin/Views/MainView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Prueba_Xamarin.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainView : ContentPage
+    {
+        int count = 0;
+        public MainView()
+        {
+            InitializeComponent();
+
+
+            MyButton.Clicked += (sender, args) =>
+            {
+                LblClicks.Text = String.Format("Botón pulsado {0} veces.", ++count);
+            };
+        }
+    }
+}
